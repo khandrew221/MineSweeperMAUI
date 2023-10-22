bac8a3d1f3be814e45f5f7cfa6fe97b032ee7221
K Andrew <142013251+khandrew221@users.noreply.github.com>
2023-10-22 03:31:42 +0100

    Beginning of project. Code incomplete.

--- MineSweeperMAUI/MainPage.xaml.cs
+++ MineSweeperMAUI/MainPage.xaml.cs
@@ -1,24 +1,84 @@
-﻿namespace MineSweeperMAUI
+﻿
+namespace MineSweeperMAUI
 {
     public partial class MainPage : ContentPage
     {
+        //Variables required by the view
         int count = 0;
+        Grid grid = new Grid();
 
         public MainPage()
         {
             InitializeComponent();
+
+            // Make the game. TEMPORARY, NEEDS CONTROLLER
+            MineSweeper game = new MineSweeper(20, 20);
+
+            // Make the view's grid of buttons
+            MakeButtonGrid(20, 20, 30);
+
+            //and add it to the view
+            VerticalStackLayout lay = (VerticalStackLayout)FindByName("grid1");
+            lay.Add(grid);
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        /// <summary>
+        /// Creates the interactive button grid that forms the main view of and method of with the game field.
+        /// </summary>
+        /// <param name="width">int, >0 (work on better constraints/recommended values later)</param>
+        /// <param name="height">int, >0 (work on better constraints/recommended values later)</param>
+        /// <param name="buttonSize">int, >0 (work on better constraints/recommended values later)</param>
+        /// <param name="game"> TEMPORARY, should link to MAUI adapter/controller not direct to model class</param>
+        private void MakeButtonGrid(int width, int height, int buttonSize)
         {
-            count++;
+            grid = new Grid();
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            for (int i = 0; i < width; i++)
+            {
+                grid.AddColumnDefinition(new ColumnDefinition { Width = new GridLength(buttonSize) });
+            }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            for (int i = 0; i < height; i++)
+            {
+                grid.AddRowDefinition(new RowDefinition { Height = new GridLength(buttonSize) });
+            }
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    GridButton btn = new GridButton(j * width + i);
+                    btn.Text = "?";
+                    btn.IsEnabled = true;
+                    grid.Add(btn, i, j);
+                }
+            }
         }
     }
+
+    /// <summary>
+    /// The buttons that make up the interactive cell grid in the view. 
+    /// </summary>
+    class GridButton : Button
+    {
+        int cellNum;
+
+        public GridButton(int cellnum)
+        {
+            //Sets the default style for GridButton objects.
+            // !!! Figure out how to do with styles.xaml !!! 
+            VerticalOptions = LayoutOptions.Fill;
+            HorizontalOptions = LayoutOptions.Fill;
+            BorderColor = Colors.Black;
+            CornerRadius = 0;
+            Clicked += new System.EventHandler(GridButtonClicked);
+            this.cellNum = cellnum;
+        }
+
+        private void GridButtonClicked(object sender, EventArgs e)
+        {
+            // code goes here
+        }
+    };
 }
--- MineSweeperMAUI/MineSweeper.cs
+++ MineSweeperMAUI/MineSweeper.cs
@@ -6,9 +6,175 @@
 /// </summary>
 class MineSweeper
 {
+    /// <summary>
+    /// Handles the grid of cells that make up the game
+    /// </summary>
+    private CellGrid cellGrid;
+    /// <summary>
+    /// Reports on the state of the game: 0 for active, -1 for loss state, 1 for a win state 
+    /// </summary>
+    public int GameOver { get; private set; }
 
+    /// <summary>
+    /// Class constructor, initialising a grid of the specified width and height. Grid size can
+    /// be changed later (!!!not currently)
+    /// </summary>
+    public MineSweeper(int width, int height)
+    {
+        cellGrid = new CellGrid(width, height);
+    }
 
+    /// <summary>
+    /// Encapsulates the storage and processing of individual cells. 
+    /// Due to the cells being stored as a single dimensional array, a new CellGrid must be 
+    /// created whenever the grid size is changed. 
+    /// </summary>
+    class CellGrid
+    {
+        /// <summary>
+        /// Width of the grid.
+        /// </summary>
+        readonly int WIDTH;
+        /// <summary>
+        /// Height of the grid.
+        /// </summary>
+        readonly int HEIGHT;
+        /// <summary>
+        /// Array holding the Cell objects that make up the grid
+        /// </summary>
+        private Cell[] cells;
 
+        /// <summary>
+        /// Cell grid constructor. Note that this does not set up a new game state, only
+        /// a grid of empty cells with neighbours set. The grid's size is fixed for its lifetime;
+        /// a new CellGrid must be made to change grid size. This is a measure to prevent mismatches
+        /// occuring between WIDTH, HEIGHT, cells, and cell neighbour settings at any point: they
+        /// should all be fixed on creation and never change.
+        /// </summary>
+        /// <param name="width">The grid width. Precondition: >0.</param>
+        /// <param name="height">The grid height. Precondition: >0.</param>
+        public CellGrid(int width, int height) 
+        {
+            //set width and height
+            WIDTH = width;
+            HEIGHT = height;
+            cells = new Cell[WIDTH * HEIGHT];
+
+            //create the array of cells
+            for (int i = 0; i < HEIGHT * WIDTH; i++)
+            {
+                cells[i] = new Cell();
+            }
+
+            //set cell neighbours
+            for (int x = 0; x < WIDTH; x++)
+            {
+                for (int y = 0; y < HEIGHT; y++)
+                {
+                    //get the index of the cell in the array
+                    int cellIndex = XYToIndex(x, y);
+
+                    //assign an empty neighbour set for the cell
+                    cells[cellIndex].neighbours = new HashSet<Cell>();
+
+                    //check direct left for a cell, and add it as a neighbour if it exists
+                    int neighbourIndex = XYToIndex(x - 1, y);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //same for lower left
+                    neighbourIndex = XYToIndex(x - 1, y - 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //and upper left
+                    neighbourIndex = XYToIndex(x - 1, y + 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //check direct right
+                    neighbourIndex = XYToIndex(x + 1, y);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //same for lower right
+                    neighbourIndex = XYToIndex(x + 1, y - 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //and upper right
+                    neighbourIndex = XYToIndex(x + 1, y + 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //directly above
+                    neighbourIndex = XYToIndex(x, y + 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+
+                    //and directly below
+                    neighbourIndex = XYToIndex(x, y - 1);
+                    if (neighbourIndex != -1)
+                    {
+                        cells[cellIndex].neighbours.Add(cells[neighbourIndex]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts an (x,y) value to the index of the cell at that position, or -1 if the (x,y) value is not valid.
+        /// </summary>
+        /// <param name="x">The x value to be converted. Out of bounds values are caught and return an error.</param>
+        /// <param name="y">The x value to be converted. Out of bounds values are caught and return an error.</param>
+        /// <returns>The index of the cell at this x,y location, otherwise -1 as an error value.</returns>
+        int XYToIndex(int x, int y)
+        {
+            if (0 <= x && x < WIDTH && 0 <= y && y < HEIGHT)
+            {
+                //if the x and y values are within the grid, return an index
+                return y * WIDTH + x;
+            } 
+            else
+            { 
+                //otherwise, return an error value
+                return -1; 
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handles the state of individual cells in the game grid
+    /// </summary>
+    public class Cell
+    {
+        /// <summary>
+        /// Whether or not the cell is hidden.
+        /// </summary>
+        public bool isHidden = true;
+        /// <summary>
+        ///  Whether or not the cell is a bomb.
+        /// </summary>
+        public bool isBomb = false;
+        /// <summary>
+        /// A reference to each directly neighbouring cell, including diagonals
+        /// </summary>
+        public HashSet<Cell> neighbours;
+    }
 
 
 }
